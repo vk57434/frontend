9b06df2eba6db188e15c1524d0652a13a960e9aa
vk57434 <vk4492020@gmail.com>
2026-03-31 23:42:54 +0530

    add env

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://golf-backend1.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
